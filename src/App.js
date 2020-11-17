f5583d6582e1a7d4335dad88e37fb8787a3ac6dc
Flávio Neto <flavioneto96@gmail.com>
2020-11-17 16:38:05 -0300

    update our menu-item to navigate with routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/shop/hats" exact>
+            <HatsPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size || ''} menu-item`}>
-    <div className="background-image" 
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }} />
-    <div className="content">
-      <h1 className="title">{title}</h1>
-      <span className="subtitle">SHOP NOW</span>
-    </div>
-  </div>
-);
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
+  const itemClass = size ? `${size} menu-item` : 'menu-item';
 
-export default MenuItem;
+  return (
+    <div className={itemClass} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+      <div className="background-image" 
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }} />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+}
+
+export default withRouter(MenuItem);
