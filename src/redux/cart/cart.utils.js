9097196ec50f66c51cfe5020a8b63ec1cb8d2816
Flávio Neto <flavioneto96@gmail.com>
2021-01-08 14:32:05 -0300

    created cart-item component and inserted in cart-dropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,7 +2,7 @@ export const addItemToCart = (cart, itemToAdd) => {
   const itemAlreadyOnCart = cart.find((item) => item.id === itemToAdd.id); // ver se ele ja ta no carrinho
   if (itemAlreadyOnCart) {
     itemAlreadyOnCart.quantity += 1; // modifica por referencia, pega o objeto original
-    return cart;
+    return [...cart];
   } else {
     return [...cart, { ...itemToAdd, quantity: 1 }]; // retorna um array novo
   }
